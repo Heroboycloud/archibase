81cc747d96fd69ce543bc221dcaae513275c052c
Heroboycloud <shakiratakindel@gmail.com>
2026-01-25 02:54:33 +0100

    Edited Dashboard.php with the widgets

--- dashboard.php
+++ dashboard.php
@@ -28,7 +28,7 @@ include_once "header.php";
           <h3>99</h3>
         </div>
         <div class="w3-clear"></div>
-        <h4>Views</h4>
+        <h4>Users</h4>
       </div>
     </div>
     <div class="w3-quarter">
@@ -38,20 +38,47 @@ include_once "header.php";
           <h3>23</h3>
         </div>
         <div class="w3-clear"></div>
-        <h4>Shares</h4>
+        <h4>Ai answers</h4>
       </div>
     </div>
     <div class="w3-quarter">
-      <div class="w3-container w3-orange w3-text-white w3-padding-16">
+      <div class="w3-container w3-teal w3-padding-16">
+        <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>23</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Keypoints</h4>
+      </div>
+    </div>
+    <div class="w3-quarter w3-margin-top">
+      <div class="w3-container w3-orange w3-text-white w3-padding-16 ">
         <div class="w3-left"><i class="fa fa-users w3-xxxlarge"></i></div>
         <div class="w3-right">
           <h3>50</h3>
         </div>
         <div class="w3-clear"></div>
-        <h4>Users</h4>
+        <h4>Files</h4>
+      </div>
+    </div>
+    <div class="w3-quarter w3-margin-top">
+      <div class="w3-container w3-orange w3-text-white w3-padding-16 ">
+        <div class="w3-left"><i class="fa fa-users w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>50</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Courses</h4>
       </div>
     </div>
   </div>
+  
+<div class="w3-panel w3-animate-bottom w3-blue w3-display-container">
+  <span onclick="this.parentElement.style.display='none'"
+  class="w3-button w3-large w3-display-topright">&times;</span>
+  <h3>Info!</h3>
+  <p>Blue often indicates a neutral informative change or action.</p>
+</div>
 
 
 </div>
